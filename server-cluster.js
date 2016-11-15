3c0da7e98e8d6d507c24eb87b372883e7b0ede83
sd2924308 <3789463@qq.com>
2016-11-15 20:45:00 +0800

    demo

--- server-cluster.js
+++ server-cluster.js
@@ -4,14 +4,14 @@ var cluster = require('cluster');
 var workers = 2;
 
 if (cluster.isMaster) {
-    for (var i = 0; i < workers; i++) {
-        cluster.fork();
-    }
+  for (var i = 0; i < workers; i++) {
+    cluster.fork();
+  }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log('worker %s died, restarting...', worker.process.pid);
-        cluster.fork();
-    });
+  cluster.on('exit', (worker, code, signal) => {
+    console.log('worker %s died, restarting...', worker.process.pid);
+    cluster.fork();
+  });
 } else {
-    require('./server.js')
+  require('./server.js')
 }
